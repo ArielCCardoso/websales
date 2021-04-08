af39f8f5c54d807e17a1ecf2e0591c5eb8ccb983
Ariel C. Cardoso <arielccardoso@live.com>
2021-04-07 23:19:16 -0300

    Seller service

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using CCardoso.SalesWeb.Data;
+using CCardoso.SalesWeb.Services;
 
 namespace CCardoso.SalesWeb
 {
@@ -42,6 +43,7 @@ namespace CCardoso.SalesWeb
             });
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
