6d59558c69a585320d6ff480c6846d01ba2d3253
Ariel C. Cardoso <arielccardoso@live.com>
2021-06-20 22:49:55 -0300

    busca agrupada

--- Models/SalesRecord.cs
+++ Models/SalesRecord.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using CCardoso.SalesWeb.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace CCardoso.SalesWeb.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus SaleStatus { get; set; }
         public Seller Seller { get; set; }
